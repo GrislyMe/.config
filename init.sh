e584030c0c8f6f3a444a8cb7cb95afe9c98de7c0
GrislyMe <GrislyMe@csie.io>
2020-11-27 23:18:01 +0800

    naming bug fix

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-rm ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.xintrc
+rm ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.xinitrc
 ln -sr ~/.config/vim/vimrc ~/.vimrc
 ln -sr ~/.config/tmux/tmux.conf ~/.tmux.conf 
 ln -sr ~/.config/zsh/zshrc ~/.zshrc
-ln -sr ~/.config/X11/xintrc ~/.xintrc
+ln -sr ~/.config/X11/xintrc ~/.xinitrc
