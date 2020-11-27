64daab05bb95732caaa159073675e4d240cabc20
GrislyMe <GrislyMe@csie.io>
2020-11-27 23:22:01 +0800

    lol

--- init.sh
+++ init.sh
@@ -2,4 +2,4 @@ rm ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.xinitrc
 ln -sr ~/.config/vim/vimrc ~/.vimrc
 ln -sr ~/.config/tmux/tmux.conf ~/.tmux.conf 
 ln -sr ~/.config/zsh/zshrc ~/.zshrc
-ln -sr ~/.config/X11/xintrc ~/.xinitrc
+ln -sr ~/.config/X11/xinitrc ~/.xinitrc
