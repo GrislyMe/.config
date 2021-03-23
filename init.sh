6d1548288dabf5fd3e80105a9c089d04cd43dd40
GrislyMe <GrislyMe@csie.io>
2021-03-23 09:37:24 +0800

    urxvt config

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
-rm ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.xinitrc
+rm ~/.vimrc ~/.tmux.conf ~/.zshrc ~/.xinitrc ~/.Xresources
 ln -sr ~/.config/vim/vimrc ~/.vimrc
 ln -sr ~/.config/tmux/tmux.conf ~/.tmux.conf 
 ln -sr ~/.config/zsh/zshrc ~/.zshrc
 ln -sr ~/.config/X11/xinitrc ~/.xinitrc
+ln -sr ~/.config/Xresources/Xresources ~/.Xresources
